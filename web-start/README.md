7cb7f20552e76fbfed21e7f0aa2e4692393e1bbb
efg.river <efg.river@gmail.com>
2019-07-21 14:53:59 +0900

    friendlychat codelab done

--- web-start/README.md
+++ web-start/README.md
@@ -1,5 +1,102 @@
-# Firebase Web Codelab - Start code
+# README
 
-This folder contains the starting code for the [Firebase: Build a Real Time Web Chat App Codelab](https://codelabs.developers.google.com/codelabs/firebase-web/).
+![capture](./capture.png)
 
-If you'd like to jump directly to the end and see the finished code head to the [web](../web) directory.
+## Commands
+
+```
+$ npm -g install firebase-tools
+$ firebase --version
+$ firebase login
+$ firebase use --add
+$ firebase serve
+$ firebase deploy
+```
+
+## Output
+
+https://friendlychat-f6c3a.firebaseapp.com
+
+## 詰まった点
+* Cloud Authentication
+  Signin しようとすると、403エラーが表示されて、Google Provider認証がエラーになる-> OAuthへ同意する。アプリケーション ホームページ] リンクと[アプリケーション プライバシー ポリシー] リンクが必要
+  - [API とサービス / 認証情報](https://console.developers.google.com/apis/credentials/consent?project={xxxx})
+
+* Cloud FireStore
+  * エラー `writing new message to Firebase Database FirebaseError: Missing or insufficient permissions` の対処
+    - Filre store ruleで request.authがnullではない場合だけ許可するように書き換え
+    - test modeで作っていれば発生しなかった模様だが、
+    - Firestoreのデータベースを初期設定する際のモード選択で、lock modeを選んで作成したことで発生
+    - FirestoreのRule初期設定が、test mode(Codelabで指示された方)を選んだ場合とlock modeを選んだ場合で異なる模様
+       - https://console.firebase.google.com/u/1/project/friendlychat-f6c3a/database/firestore/rules
+
+    - 以下のように変更した
+    
+    ```
+    rules_version = '2';
+    service cloud.firestore {
+        match /databases/{database}/documents {
+            match /{document=**} {
+                allow read, write: if false;
+            }
+        }
+    }
+
+    ↓
+
+    rules_version = '2';
+    service cloud.firestore {
+    match /databases/{database}/documents {
+        match /{document=**} {
+        allow read, write: if request.auth != null;
+        }
+    }
+    }
+    ```
+
+  * Firestore内のデータ構造の単位
+    - collections, documents, fields, and subcollections
+  * FirestoreにSDK経由で最初のメッセージ書き込みを行ったタイミングで自動でmessagesコレクションが作成されたのを確認した
+     - messagesコレクションは、あらかじめFireBaseのコンソールで作っておかなくてもOK。
+        - messagesコレクションはConsoleで作成する場合は、１つ目のdocumentを作成させられる。
+  
+
+* Developer Console Memo
+
+```
+[2019-07-21T04:09:44.477Z]  @firebase/firestore: Firestore (6.3.0): 
+  The timestampsInSnapshots setting now defaults to true and you no
+  longer need to explicitly set it. In a future release, the setting
+  will be removed entirely and so it is recommended that you remove it
+  from your firestore.settings() call now.
+```
+
+* Cloud Storage:  is better suited for storing files
+
+* Cloud Messaging 
+  - publicディレクトとりに配置するmanifest.jsonファイルの中に記載するgcm_sender_idはFirebase Cloud Messagingを使う場合、 `103953800507` 
+    - 変えてはいけない
+
+```
+Unable to get messaging token. FirebaseError: Messaging: We are unable to register the default service worker. Failed to register a ServiceWorker: A bad HTTP response code (404) was received when fetching the script. (messaging/failed-serviceworker-registration).
+```
+
+* Performance Monitoring
+
+  - 早速FirebaseのConsole上で確認してみたが、表示されない。
+    - 最初のデータは 12時間程度待つと表示されるとのこと。
+  - そもそもログが飛んでいるのかの確認は、ChromeのDeveloperコンソールでログをみる
+    - FriendlychatをデプロイしたfirebaseのホストURLをChromeで開く
+      - https://friendlychat-f6c3a.firebaseapp.com
+    - ChromeのDeveloperコンソールを開いて、Networkタブをチェック
+      - firebaselogging.googleapis.comに対してリクエストが送信されていることを確認できればOK
+         - https://firebaselogging.googleapis.com/v0cc/log?format=json_proto
+    - 送信されてなければ、以下のfirebase-performance.jsの読み込みとオブジェクトの初期化が行われてるか再確認
+
+```
+<script src="/__/firebase/6.3.0/firebase-performance.js"></script>
+<script>
+  var perf = firebase.performance();
+</script>
+```
+    - 
--- web-start/public/scripts/main.js
+++ web-start/public/scripts/main.js
@@ -18,57 +18,149 @@
 // Signs-in Friendly Chat.
 function signIn() {
   // TODO 1: Sign in Firebase with credential from the Google user.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
 }
 
 // Signs-out of Friendly Chat.
 function signOut() {
   // TODO 2: Sign out of Firebase.
+  firebase.auth().signOut();
 }
 
 // Initiate firebase auth.
 function initFirebaseAuth() {
   // TODO 3: Initialize Firebase.
+  firebase.auth().onAuthStateChanged(authStateObserver);
 }
 
 // Returns the signed-in user's profile Pic URL.
 function getProfilePicUrl() {
   // TODO 4: Return the user's profile pic URL.
+  return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
 }
 
 // Returns the signed-in user's display name.
 function getUserName() {
   // TODO 5: Return the user's display name.
+  return firebase.auth().currentUser.displayName;
 }
 
 // Returns true if a user is signed-in.
 function isUserSignedIn() {
   // TODO 6: Return true if a user is signed-in.
+  return !!firebase.auth().currentUser;
 }
 
-// Saves a new message on the Firebase DB.
+// Saves a new message on the Cloud Firestore.
 function saveMessage(messageText) {
-  // TODO 7: Push a new message to Firebase.
+  console.log("Hey there!");
+  // Add a new message entry to the Firebase database.
+  return firebase.firestore().collection('messages').add({
+    name: getUserName(),
+    text: messageText,
+    profilePicUrl: getProfilePicUrl(),
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+  }).catch(function(error) {
+    console.error('Error writing new message to Firebase Database', error);
+  });
 }
 
 // Loads chat messages history and listens for upcoming ones.
 function loadMessages() {
+    // Create the query to load the last 12 messages and listen for new ones.
+    var query = firebase.firestore()
+    .collection('messages')
+    .orderBy('timestamp', 'desc')
+    .limit(12);
   // TODO 8: Load and listens for new messages.
+    // Start listening to the query.
+
+  query.onSnapshot(function(snapshot) {
+    snapshot.docChanges().forEach(function(change) {
+      if (change.type === 'removed') {
+        console.log("remove");
+        console.log(change);
+        deleteMessage(change.doc.id);
+      } else {
+        console.log("NOT remove but changed");
+        console.log(change);
+        var message = change.doc.data();
+        displayMessage(change.doc.id, message.timestamp, message.name,
+                        message.text, message.profilePicUrl, message.imageUrl);
+      }
+    });
+  });
 }
 
 // Saves a new message containing an image in Firebase.
 // This first saves the image in Firebase storage.
 function saveImageMessage(file) {
-  // TODO 9: Posts a new image as a message.
+
+  // 1 - We add a message with a loading icon that will get updated with the shared image.
+  firebase.firestore().collection('messages').add({
+    name: getUserName(),
+    imageUrl: LOADING_IMAGE_URL,
+    profilePicUrl: getProfilePicUrl(),
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+  }).then(function(messageRef) {
+    console.log("currentUser.uid");
+    console.log(firebase.auth().currentUser.uid);
+    console.log("messageRef.id");
+    console.log(messageRef.id);
+    console.log("file.name");
+    console.log(file.name);
+    
+    // 2 - Upload the image to Cloud Storage.
+    var filePath = firebase.auth().currentUser.uid + '/' + messageRef.id + '/' + file.name;
+    console.log("FilePath");
+    console.log(filePath);
+    return firebase.storage().ref(filePath).put(file).then(function(fileSnapshot) {
+      // 3 - Generate a public URL for the file.
+      return fileSnapshot.ref.getDownloadURL().then((url) => {
+        console.log("Download URL");
+        console.log(url);
+        // 4 - Update the chat message placeholder with the image's URL.
+        return messageRef.update({
+          imageUrl: url,
+          storageUri: fileSnapshot.metadata.fullPath
+        }).then(function(result) {
+
+          console.log(result);
+        });
+      });
+    });
+  }).catch(function(error) {
+    console.error('There was an error uploading a file to Cloud Storage:', error);
+  });
 }
 
 // Saves the messaging device token to the datastore.
 function saveMessagingDeviceToken() {
-  // TODO 10: Save the device token in the realtime datastore
+  firebase.messaging().getToken().then(function(currentToken) {
+    if (currentToken) {
+      console.log('Got FCM device token:', currentToken);
+      // Saving the Device Token to the datastore.
+      firebase.firestore().collection('fcmTokens').doc(currentToken)
+          .set({uid: firebase.auth().currentUser.uid});
+    } else {
+      // Need to request permissions to show notifications.
+      requestNotificationsPermissions();
+    }
+  }).catch(function(error){
+    console.error('Unable to get messaging token.', error);
+  });
 }
 
 // Requests permissions to show notifications.
 function requestNotificationsPermissions() {
-  // TODO 11: Request permissions to send notifications.
+  console.log('Requesting notifications permission...');
+  firebase.messaging().requestPermission().then(function() {
+    // Notification permission granted.
+    saveMessagingDeviceToken();
+  }).catch(function(error) {
+    console.error('Unable to get permission to notify.', error);
+  });
 }
 
 // Triggered when a file is selected via the media picker.
@@ -81,7 +173,7 @@ function onMediaFileSelected(event) {
 
   // Check if the file is an image.
   if (!file.type.match('image.*')) {
-    var data = {
+    var data = {       
       message: 'You can only share images',
       timeout: 2000
     };
@@ -97,6 +189,7 @@ function onMediaFileSelected(event) {
 // Triggered when the send new message form is submitted.
 function onMessageFormSubmit(e) {
   e.preventDefault();
+  console.log("Send message")
   // Check that the user entered a message and is signed in.
   if (messageInputElement.value && checkSignedInWithMessage()) {
     saveMessage(messageInputElement.value).then(function() {
@@ -289,7 +382,10 @@ initFirebaseAuth();
 
 // Remove the warning about timstamps change. 
 var firestore = firebase.firestore();
-var settings = {timestampsInSnapshots: true};
+
+// TODO: The timestampsInSnapshots setting now defaults to true and you no longer need to explicitly set it.
+// var settings = {timestampsInSnapshots: true};
+var settings = {};
 firestore.settings(settings);
 
 // TODO: Enable Firebase Performance Monitoring.
